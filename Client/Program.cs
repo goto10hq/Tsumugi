6a1c06de0cd33721fe0b64d4cc5da788e94a0847
frohikey <frohikey@gmail.com>
2019-02-03 14:37:51 +0100

    Version 2.0.0

---
+++ Client/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Momo.Tokens;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Tsumugi;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var appSettings = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                //.AddJsonFile("AppSettings.json")
+                .AddUserSecrets("tsumugi")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var configuration = new TsumugiConfiguration(appSettings["Tsumugi:ConnectionString"],
+                appSettings["Tsumugi:Queue"],
+                appSettings["Tsumugi:Channel"],
+                appSettings["Tsumugi:Emoji"],
+                appSettings["Tsumugi:Bot"]);
+
+            var bus = new Bus(configuration);
+
+            bus.ErrorHandler += exception => Console.WriteLine($"Error: {exception.Message}.");
+
+            await bus.SendAsync("Aloha 2 *dudes*!");
+            //bus.Send("general", ":panda_face:", "Tsumugi", "Wellp...", new List<Attachment>
+            //                                                           {
+            //                                                               new Attachment
+            //                                                               {
+            //                                                                   Fields = new List<Field>
+            //                                                                            {
+            //                                                                                new Field("ID", "1", true),
+            //                                                                                new Field("Name", "Sushi", true)
+            //                                                                            }
+            //                                                               },
+            //                                                               new Attachment
+            //                                                               {
+            //                                                                   Fields = new List<Field>
+            //                                                                            {
+            //                                                                                new Field("ID", "2", true),
+            //                                                                                new Field("Name", "Sashimi", true)
+            //                                                                            }
+            //                                                               }
+            //                                                           });
+        }
+    }
+}
---
+++ Tsumugi/Bus.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Momo.Tokens;
+using Newtonsoft.Json;
+
+namespace Tsumugi
+{
+    public class Bus
+    {
+        private readonly string _connectionString;
+        private readonly string _queue;
+        private readonly string _channel;
+        private readonly string _emoji;
+        private readonly string _bot;
+        private readonly QueueClient _queueClient;
+
+        public Action<Exception> ErrorHandler { get; set; }
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        /// <param name="tsumugiConfiguration">Tsumugi configuration.</param>
+        public Bus(ITsumugiConfiguration tsumugiConfiguration) : this(tsumugiConfiguration.ConnectionString,
+            tsumugiConfiguration.Queue,
+            tsumugiConfiguration.Channel,
+            tsumugiConfiguration.Emoji,
+            tsumugiConfiguration.Bot)
+        {
+        }
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        /// <param name="connectionString">Connection string.</param>
+        /// <param name="queue">Queue.</param>
+        /// <param name="channel">Channel.</param>
+        /// <param name="emoji">Emoji.</param>
+        /// <param name="bot">Bot.</param>
+        public Bus(string connectionString,
+            string queue,
+            string channel,
+            string emoji,
+            string bot)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+            _queue = queue ?? throw new ArgumentNullException(nameof(queue));
+            _channel = channel ?? throw new ArgumentNullException(nameof(channel));
+            _emoji = emoji ?? throw new ArgumentNullException(nameof(emoji));
+            _bot = bot ?? throw new ArgumentNullException(nameof(bot));
+            _queueClient = new QueueClient(connectionString, queue);
+        }               
+
+        public async Task<bool> SendAsync(string channel, string emoji, string botName, string message, IEnumerable<Attachment> attachments = null)
+        {
+            var notification = new Notification(channel, emoji, botName, message) { Attachments = attachments };
+            return await SendHelperAsync(notification);
+        }
+
+        public async Task<bool> SendAsync(string message)
+        {
+            var notification = new Notification(_channel, _emoji, _bot, message);
+            return await SendHelperAsync(notification);
+        }
+        
+        async Task<bool> SendHelperAsync(Notification notification)
+        {
+            try
+            {
+                var message = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(notification)))
+                {
+                    ContentType = "application/json",
+                    Label = "Tsumugi"
+                };
+
+                await _queueClient.SendAsync(message);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler?.Invoke(ex);
+            }
+
+            return false;
+        }
+    }
+}
